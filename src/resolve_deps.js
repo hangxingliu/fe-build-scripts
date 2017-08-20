5136e9aa070d571809f57d9c3e2766405a0b746e
LiuYue <hangxingliu@gmail.com>
2017-08-21 07:37:02 +0800

    TODO: resolve_deps

---
+++ src/resolve_deps.js
@@ -0,0 +1,18 @@
+//@ts-check
+/// <reference path="./type.d.ts" />
+
+//@version-info-start
+const VERSION = '???';
+//@version-info-end
+
+/**
+ * @preserve
+ *
+ * This is a runnable script for resolving dependencies for building configuration
+ */
+
+function main() {
+	// TODO
+}
+
+main();
